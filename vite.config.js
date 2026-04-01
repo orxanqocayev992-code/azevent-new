2502093658d2f4fff090ce6c845840d2b3afbc1d
Orxan Qocayev <orxan.qocayev992@gmail.com>
2026-04-02 00:44:17 +0400

    PWA silindi

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "AzEvent",
-        short_name: "AzEvent",
-        description: "AzEvent event xidmətləri platforması",
-        theme_color: "#070707",
-        background_color: "#070707",
-        display: "standalone",
-        orientation: "portrait",
-        start_url: "/",
-        scope: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "/apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
